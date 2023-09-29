b28d282563a37b32982f7b2be96cc597329151eb
Ryan D Wu <ryanwunu@gmail.com>
2023-09-29 15:31:39 -0700

    update be

--- backend/submit_contact.php
+++ backend/submit_contact.php
@@ -5,8 +5,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // TODO: Save the contact information to the database or perform desired actions.
+    try {
+        // TODO: Save the contact information to the database or perform desired actions.
+        // For example, you can use PDO for database interactions.
+        
+        // Replace the following lines with your database logic.
+        $pdo = new PDO('mysql:host=localhost;dbname=your_database', 'your_username', 'your_password');
+        // Your database operations here.
 
-    // Redirect back to the contact form with a success message.
-    header("Location: contact.html?success=1");
-    exit();
+        // Redirect back to the contact form with a success message.
+        header("Location: contact.html?success=1");
+        exit();
+    } catch (PDOException $e) {
+        // Handle database connection errors or other exceptions.
+        // Log the error message and redirect with an error message.
+        error_log("Database Error: " . $e->getMessage(), 0);
+        header("Location: contact.html?error=1");
+        exit();
+    } catch (Exception $e) {
+        // Handle other exceptions if needed.
+        // Log the error message and redirect with an error message.
+        error_log("Error: " . $e->getMessage(), 0);
+        header("Location: contact.html?error=1");
+        exit();
+    }
+}
+?>
