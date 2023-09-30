8dabe0868315ef4179bdd9f4fa22c395b7579051
Ryan D Wu <ryanwunu@gmail.com>
2023-09-29 19:12:15 -0700

    fixed database

--- index.php
+++ index.php
@@ -7,24 +7,27 @@
     <h1>Contact Us</h1>
     
     <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
     // Check if the form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Check if the expected form fields are set in the $_POST array
-        if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
+        if (isset($_POST["name"]) && isset($_POST["phone"]) && isset($_POST["email"]) && isset($_POST["message"])) {
             // Retrieve form data
             $name = $_POST["name"];
+            $phone = $_POST["phone"];
             $email = $_POST["email"];
             $message = $_POST["message"];
+            
+            // Create a timestamp for the 'created_at' field
+            $created_at = date("Y-m-d H:i:s");
 
             // Create a database connection
             $servername = "localhost";  // Replace with your MySQL server name or IP address
             $username = "ryan";         // Replace with your MySQL username
-            $password = "Databasepwd";     // Replace with your MySQL password
-            $database = "cashdealData";   // Replace with your MySQL database name
+            $password = "Databasepwd";  // Replace with your MySQL password
+            $database = "cashdealData"; // Replace with your MySQL database name
 
             $conn = new mysqli($servername, $username, $password, $database);
 
@@ -34,9 +37,9 @@ ini_set('display_errors', 1);
             }
 
             // Prepare and execute an SQL INSERT statement
-            $sql = "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)";
+            $sql = "INSERT INTO contacts (name, phone, email, message, created_at) VALUES (?, ?, ?, ?, ?)";
             $stmt = $conn->prepare($sql);
-            $stmt->bind_param("sss", $name, $email, $message);
+            $stmt->bind_param("sssss", $name, $phone, $email, $message, $created_at);
 
             if ($stmt->execute()) {
                 echo "<p>Thank you for your submission!</p>";
@@ -59,13 +62,14 @@ ini_set('display_errors', 1);
         <input type="text" id="name" name="name"><br><br>
 
         <label for="phone">Phone:</label>
-        <input type="phone" id="phone" name="phone"><br><br>
+        <input type="text" id="phone" name="phone"><br><br>
 
         <label for="email">Email:</label>
         <input type="email" id="email" name="email"><br><br>
 
         <label for="message">Message:</label>
         <textarea id="message" name="message" rows="4" cols="50"></textarea><br><br>
+
         <input type="submit" value="Submit">
     </form>
 </body>
