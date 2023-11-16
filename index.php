b4450f0ff68c6be469d8d04c31ad12ed3c48beb5
Ryan D Wu <ryanwunu@gmail.com>
2023-11-15 22:33:52 -0800

    update title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./styles/main.css">
-    <title>InventoryApp</title>
+    <title>Wooloo InventoryApp</title>
 </head>
 <body>
 
